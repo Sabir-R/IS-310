fa0305ab740f64d01481816fac182bb07a588b77
Sabir Rahman <47433299+Sabir-R@users.noreply.github.com>
2024-01-08 22:51:29 -0600

    Add files via upload

---
+++ TextAnalysisWithR/code/exercise1.R
@@ -0,0 +1,23 @@
+10+5
+10-5
+
+10*1576
+15760/10
+
+10+pi
+10/pi
+
+10^2
+
+x <- 10  # Assign 10 to the variable "x"
+x - 3 # subtract 3 from x
+
+x <- 10
+x - 3 + 10 / 2
+
+sqrt(12)
+abs(-23)
+round(pi)
+
+1:10
+12:37
